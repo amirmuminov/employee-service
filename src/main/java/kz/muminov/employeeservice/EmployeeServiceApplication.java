f436df910e78ed8058c498dbd96f4ba14d4cddbe
Amir Muminov <amir_9999@mail.ru>
2020-10-03 16:02:44 +0600

    feat: add connection to discovery service

--- src/main/java/kz/muminov/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/kz/muminov/employeeservice/EmployeeServiceApplication.java
@@ -1,9 +1,13 @@
 package kz.muminov.employeeservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
